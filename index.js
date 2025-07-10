fdd7b3fec480ef12d085c91b930a624224ee9965
Livne, Igal <igal.livne@bruker.com>
2025-07-10 14:27:49 +0300

    add title

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+console.log('\033c');
+console.log("██╗  ██╗███████╗██╗     ██╗     ██████╗ ")
+console.log("██║  ██║██╔════╝██║     ██║     ██╔══██╗")
+console.log("███████║█████╗  ██║     ██║     ██████╔╝")
+console.log("██╔══██║██╔══╝  ██║     ██║     ██╔═══╝ ")
+console.log("██║  ██║███████╗███████╗███████╗██║     ")
+console.log("╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝")
+
+
+
 console.log("this is my first commit");
 console.log("this is my second commit");
 console.log("this is 3rd line")
