4a4b7453b79b883cecda9a444e02cd0541356de3
Livne, Igal <igal.livne@bruker.com>
2025-07-10 14:23:58 +0300

    add getRoman()

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-console.log("this is my I commit");
-console.log("this is my II commit");
+console.log(`this is my ${getRoman(1)} commit`);
+console.log(`this is my ${getRoman(2)} commit`);
+console.log(`this is my ${getRoman(3)} commit`);
+
+
+function getRoman(value) {
+    const romanNumerals = [
+        [1000, 'M'],
+        [900, 'CM'],
+        [500, 'D'],
+        [400, 'CD'],
+        [100, 'C'],
+        [90, 'XC'],
+        [50, 'L'],
+        [40, 'XL'],
+        [10, 'X'],
+        [9, 'IX'],
+        [5, 'V'],
+        [4, 'IV'],
+        [1, 'I']
+    ];
+
+    let result = "";
+
+    for (let [num, numeral] of romanNumerals) {
+        while (value >= num) {
+            result += numeral;
+            value -= num;
+        }
+    }
+
+    return result;
+}
